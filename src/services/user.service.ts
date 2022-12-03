ba1a0fa8673d5d91639c269c5b53c6d923ac5ffd
Edem Ziddah <contact@codevoweb.com>
2022-12-03 06:31:15 +0000

    updated

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,16 @@
-import { PrismaClient, Prisma, User } from '@prisma/client';
-import { omit } from 'lodash';
-import config from 'config';
-import redisClient from '../utils/connectRedis';
-import { signJwt } from '../utils/jwt';
+import { PrismaClient, Prisma, User } from "@prisma/client";
+import { omit } from "lodash";
+import config from "config";
+import redisClient from "../utils/connectRedis";
+import { signJwt } from "../utils/jwt";
 
-export const excludedFields = ['password', 'verified', 'verificationCode'];
+export const excludedFields = [
+  "password",
+  "verified",
+  "verificationCode",
+  "passwordResetAt",
+  "passwordResetToken",
+];
 
 const prisma = new PrismaClient();
 
@@ -44,17 +50,17 @@ export const updateUser = async (
 
 export const signTokens = async (user: Prisma.UserCreateInput) => {
   // 1. Create Session
-  redisClient.set(`${user.id}`, JSON.stringify(user), {
-    EX: config.get<number>('redisCacheExpiresIn') * 60,
+  redisClient.set(`${user.id}`, JSON.stringify(omit(user, excludedFields)), {
+    EX: config.get<number>("redisCacheExpiresIn") * 60,
   });
 
   // 2. Create Access and Refresh tokens
-  const access_token = signJwt({ sub: user.id }, 'accessTokenPrivateKey', {
-    expiresIn: `${config.get<number>('accessTokenExpiresIn')}m`,
+  const access_token = signJwt({ sub: user.id }, "accessTokenPrivateKey", {
+    expiresIn: `${config.get<number>("accessTokenExpiresIn")}m`,
   });
 
-  const refresh_token = signJwt({ sub: user.id }, 'refreshTokenPrivateKey', {
-    expiresIn: `${config.get<number>('refreshTokenExpiresIn')}m`,
+  const refresh_token = signJwt({ sub: user.id }, "refreshTokenPrivateKey", {
+    expiresIn: `${config.get<number>("refreshTokenExpiresIn")}m`,
   });
 
   return { access_token, refresh_token };
